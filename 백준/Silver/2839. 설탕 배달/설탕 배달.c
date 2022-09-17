4ccbcba5f5604e31dcd932c0802b2b5d020882e0
Yunseok Song <87839885+TheYunseokSong@users.noreply.github.com>
2022-09-18 00:24:18 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+	int N, cnt=0;
+	scanf( "%d", &N);
+	while(1)
+	{
+		if( N%5==0) 
+		{
+			cnt+=N/5;
+			break;	
+		}
+		N-=3;
+		cnt++;
+		if ( N<0) break;
+	}
+	if( N<0) printf( "-1");
+	else printf( "%d", cnt);
+	return 0;	
+}
