654a48a5891a2a291e2570994e674a7d8108342a
Yunseok <87839885+karpitony@users.noreply.github.com>
2024-09-01 13:29:53 +0900

    [Silver V] Title: 영화감독 숌, Time: 164 ms, Memory: 110272 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+cnt = 0
+result = 666
+
+while True:
+    if '666' in str(result):
+        cnt += 1
+ 
+    if cnt == N:
+        break 
+    result += 1
+ 
+print(result)
