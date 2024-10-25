e575adaf121ed17c11545aa2cf3e203487e208a8
Yunseok <87839885+karpitony@users.noreply.github.com>
2024-10-25 23:17:33 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void) {
+        int N;
+        int i=1;
+        int j;
+        int reverse=0;
+
+        scanf("%d", &N);
+        while (i > 0) {
+                if (i == N)
+                        reverse = 1;
+                        
+                for (j = 0; j < N-i; j++)
+                        printf(" ");
+                for (j = 0; j < i; j++)
+                        printf("*");
+                printf("\n");
+                
+                if(!reverse)
+                        i++;
+                else
+                        i--;
+        }
+
+        return 0;
+}
