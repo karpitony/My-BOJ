a9b66462174e849a4ea53791b39d5d0d406e5671
Yunseok <87839885+karpitony@users.noreply.github.com>
2025-09-11 00:14:32 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int T;
+	cin >> T;
+
+	vector<int> dp(12);
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 4; i < 12; i++)
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+
+	for (int i = 0; i < T; i++) {
+		int n, cnt;
+		cin >> n;
+		cout << dp[n] << "\n";
+	}
+
+	return 0;
+}
